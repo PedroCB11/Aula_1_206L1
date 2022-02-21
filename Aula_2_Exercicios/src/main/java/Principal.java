ccc6582e141bc7ef1c7c25d22dbd8904417b45e4
Pedro Bressan <pbressan11@gmail.com>
2022-02-21 09:31:36 -0300

    Exercicio 1 aula 2 c206

--- Aula_2_Exercicios/src/main/java/Principal.java
+++ Aula_2_Exercicios/src/main/java/Principal.java
@@ -1,2 +1,26 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Principal {
+
+    public static void main(String[] args) {
+        String E = "000 010 100 111";
+        String OU = "000 011 101 111";
+        String variavel;
+
+        System.out.println("qual voce deseja? E ou OU?");
+        Scanner sc = new Scanner(System.in);
+        variavel = sc.next();
+
+
+        if(variavel.equals("E")){
+            System.out.println(E);
+        }
+        else {
+            System.out.println(OU);
+        }
+    }
+
+
 }
+
